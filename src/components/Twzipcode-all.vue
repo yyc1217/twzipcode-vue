422a6f0e244684903cdf9baa0af520bd2e2fc74e
Yeh-Yung <yyc1217@gmail.com>
2017-03-15 17:05:45 +0800

    Add all component

---
+++ src/components/Twzipcode-all.vue
@@ -0,0 +1,39 @@
+<template>
+
+<select class="twzipcode__all">
+    <option v-for="zipcode in zipcodes" :value="zipcode.zipcode">{{ optionText(zipcode) }}</option>
+</select>
+
+</template>
+
+<script>
+import data from 'twzipcode-data'
+
+export default {
+    props: {
+        optionTemplate: {
+            type: String,
+            default: ':zipcode :county :city'
+        }
+    },
+    data () {
+        return {
+            zipcodes: data('zh-tw').zipcodes
+        }
+    },
+    methods: {
+        optionText ({county, city, zipcode}) {
+
+            let text = this.optionTemplate
+            text = text.replace(':county', county)
+            text = text.replace(':city', city)
+            text = text.replace(':zipcode', zipcode)
+
+            return text
+        }
+    }
+}
+</script>
+
+<style>
+</style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import All from './components/Twzipcode-all.vue'
+
+new Vue({
+  el: '#app',
+  components: {
+    TwzipcodeAll: All
+  }
+})
