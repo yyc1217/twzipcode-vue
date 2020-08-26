f9000b6a7f187cdb49437ee0091bcbb7b0fc3f60
Yeh-Yung <yyc1217@gmail.com>
2020-08-26 16:29:45 +0800

    Update demo page

--- src/demo/demo.js
+++ src/demo/demo.js
@@ -2,13 +2,6 @@ import Vue from 'vue'
 import { Zipcode, County, ZipcodeGroupby } from '../'
 import '../demo/demo.scss'
 
-// https://laracasts.com/discuss/channels/vue/use-a-global-event-bus
-const bus = new Vue({})
-
-bus.$on('twzipcode__zipcode--7:change', function (event) {
-  document.getElementById('demoCallback').textContent = event.value
-})
-
 new Vue({
   el: '#app',
   components: {
@@ -18,7 +11,8 @@ new Vue({
   },
   data () {
     return {
-      bus
+      myCounty: '臺中市',
+      myZipcode: ''
     }
   }
 })
